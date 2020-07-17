b123d4c7214006ded6da11639d91f4dbbfe8b197
Sayali Ovhal <ovhalsayali@gmail.com>
2020-07-17 16:50:27 +0530

    added customer table

---
+++ mssql/mssql/dbo/Tables/customers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[customers] (
+    [customer_id]        INT          NOT NULL,
+    [first_name]         VARCHAR (30) NOT NULL,
+    [last_name]          VARCHAR (30) NOT NULL,
+    [date_of_birth]      DATE         NOT NULL,
+    [gender]             VARCHAR (10) NOT NULL,
+    [age]                INT          NOT NULL,
+    [country]            VARCHAR (30) NOT NULL,
+    [last_purchase_date] DATE         NULL,
+    [customer_status]    TINYINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([customer_id] ASC)
+);
+
